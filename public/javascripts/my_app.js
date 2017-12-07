27a3927d4c32bd232d2ef933979330959c37d878
Daniel Kindt <danieltkindt@gmail.com>
2017-12-07 13:48:48 -0700

    initial commit for the project; still adding over the pieces from the last project. right now, this is just lab6...

---
+++ public/javascripts/my_app.js
@@ -0,0 +1,12 @@
+controller('myController', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get('/user/profile')
+            .success(function(data, status, headers, config) {
+                $scope.user = data;
+                $scope.error = "";
+            }).
+        error(function(data, status, headers, config) {
+            $scope.user = {};
+            $scope.error = data;
+        });
+    }]);
